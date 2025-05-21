90a49ed4673efcff0d77782d94a383daeb2dd1cf
ankurdang5 <ankurdang5@gmail.com>
2025-05-22 00:44:16 +0530

    adding solution

---
+++ GetStockUpdate/GetStockUpdate/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetStockUpdate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockUpdateController : ControllerBase
+    {
+        //Code to call the API and get the stock update
+    }
+}
